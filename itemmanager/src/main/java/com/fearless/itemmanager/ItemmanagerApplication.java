1c217b5779985731d9e04d15d4a6bc50e1af66a4
umaircha3 <umair2@umbc.edu>
2022-12-15 14:41:09 -0500

    Initial commit

---
+++ itemmanager/src/main/java/com/fearless/itemmanager/ItemmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fearless.itemmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemmanagerApplication.class, args);
+	}
+
+}
---
+++ itemmanager/src/test/java/com/fearless/itemmanager/ItemmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fearless.itemmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
